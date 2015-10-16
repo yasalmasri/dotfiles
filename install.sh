2080f107b2a1ff9e79d0b3800a8b85c93a1be089
Yaser Almasri <yaserms@outlook.com>
2015-10-16 10:43:00 -0500

    add gitignore_global

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ git submodule update
 
 # vim +BundleInstall +qall
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluingInstall +qall
 
 echo source ~/.dotfiles/vim-for-rails.vim  >> ~/.vimrc
 echo source ~/.dotfiles/gvim-for-rails.vim >> ~/.gvimrc
 echo source ~/.dotfiles/profile            >> ~/.profile
 echo source ~/.dotfiles/profile            >> ~/.bashrc
 echo source ~/.dotfiles/zsh                >> ~/.zshrc
-
-vim +PluingInstall +qall
+git config --global core.excludesfile ~/.dotfiles/gitignore_global
