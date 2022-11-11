f3223d6fbde976b0eee5a32f865575475da49773
Yaser Almasri <yaser@masys.mx>
2022-11-11 10:25:50 -0600

    nvim surround

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -232,6 +232,16 @@ return packer.startup(function(use)
     end
   }
 
+  use({
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  })
+
   --------------------
   -- Autocompletion --
   --------------------
