74b159c79489983a8d89103af07b150eea8719a0
Yaser Almasri <yaser.almasri@apptegy.com>
2023-01-25 22:07:43 -0600

    disable ruby, specify langs

--- config/nvim/lua/configs/treesitter.lua
+++ config/nvim/lua/configs/treesitter.lua
@@ -7,7 +7,10 @@ end
 -- configure treesitter
 treesitter.setup({
   -- enable syntax highlighting
-  highlight = { enable = true },
+  highlight = {
+    enable = true,
+    disable = { "ruby" }
+  },
   -- enable indentation
   indent = {
     enable = true,
@@ -16,7 +19,15 @@ treesitter.setup({
   -- enable autotagging (w/ nvim-ts-autotag plugin)
   autotag = { enable = true },
   -- ensure these language parsers are installed
-  ensure_installed = 'all',
+  -- ensure_installed = "all",
+  ensure_installed = {
+    "help",
+    "c",
+    "lua",
+    "vim",
+    "ruby",
+    "javascript"
+  },
   -- auto install above language parsers
   auto_install = true,
 })
