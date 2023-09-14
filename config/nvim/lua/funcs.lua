be81997d510d1a920e4cdfbaf0f822f07b0aceab
Yaser Almasri <yaser@masys.mx>
2023-09-14 00:55:47 -0600

    hash, json funcs

---
+++ config/nvim/lua/funcs.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_user_command('ParseJSON',function()
+  vim.cmd([[:%s/\\n/\r/ge]])
+  vim.cmd([[:%s/\\"/"/ge]])
+end,{})
+
+vim.api.nvim_create_user_command('HashToJSON',function()
+  vim.cmd([[:%s/"=>"/":"/ge]])
+  vim.cmd([[:%s/" => "/":"/ge]])
+  vim.cmd([[:%s/"=> "/":"/ge]])
+  vim.cmd([[:%s/" =>"/":"/ge]])
+end,{})
