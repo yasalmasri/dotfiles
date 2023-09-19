0e18ccdeb0604b286adef000ebe9ff2dfc7ea73d
Yaser Almasri <yaser@masys.mx>
2023-09-18 22:00:40 -0600

    w, q commands

--- config/nvim/lua/funcs.lua
+++ config/nvim/lua/funcs.lua
@@ -9,3 +9,15 @@ vim.api.nvim_create_user_command('HashToJSON',function()
   vim.cmd([[:%s/"=> "/":"/ge]])
   vim.cmd([[:%s/" =>"/":"/ge]])
 end,{})
+
+vim.api.nvim_create_user_command('W',function()
+  vim.cmd([[:w]])
+end,{})
+
+vim.api.nvim_create_user_command('Wq',function()
+  vim.cmd([[:wq]])
+end,{})
+
+vim.api.nvim_create_user_command('Q',function()
+  vim.cmd([[:q]])
+end,{})
