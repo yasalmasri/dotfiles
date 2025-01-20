ff063d93f69608e5fe764c184ca79dcb40ac965c
Yaser Almasri <yaser.almasri@apptegy.com>
2025-01-20 15:38:45 -0600

    config

--- config/nvim/after/plugin/mini.starter.lua
+++ config/nvim/after/plugin/mini.starter.lua
@@ -1,2 +1,27 @@
 MiniDeps.add('echasnovski/mini.starter')
-require('mini.starter').setup()
+  local my_items = {
+    { name = 'Echo random number', action = 'lua print(math.random())', section = 'Section 1' },
+    function()
+      return {
+        { name = 'Item #1 from function', action = [[echo 'Item #1']], section = 'From function' },
+        { name = 'Placeholder (always inactive) item', action = '', section = 'From function' },
+        function()
+          return {
+            name = 'Item #1 from double function',
+            action = [[echo 'Double function']],
+            section = 'From double function',
+          }
+        end,
+      }
+    end,
+    { name = [[Another item in 'Section 1']], action = 'lua print(math.random() + 10)', section = 'Section 1' },
+  }
+
+  local starter = require('mini.starter')
+  starter.setup({
+    items = my_items,
+    content_hooks = {
+      starter.gen_hook.adding_bullet("- "),
+      starter.gen_hook.aligning("center", "center")
+    },
+  })
