2206fdfe0be0a9aaf5fad4941ecb3469edf4f5e8
Yaser Almasri <yaser.almasri@apptegy.com>
2025-06-10 08:30:10 -0600

    indent blankline

---
+++ config/nvim/after/plugin/indent-blankline.lua
@@ -0,0 +1,15 @@
+MiniDeps.add({ source = 'lukas-reineke/indent-blankline.nvim', })
+
+require("ibl").setup({
+  indent = { char = ":" },
+  scope = {
+    enabled = false,
+  },
+})
+MiniDeps.now(function()
+  MiniDeps.now(function()
+    -- asdasda
+    -- asdasd
+    -- asdasd
+  end)
+end)
