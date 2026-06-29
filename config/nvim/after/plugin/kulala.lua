774e49ea2974277d0599d8d0a686c0980485c483
Yaser Almasri <yaser@masys.mx>
2026-06-29 14:49:01 -0600

    kulala

--- config/nvim/after/plugin/kulala.lua
+++ config/nvim/after/plugin/kulala.lua
@@ -1,4 +1,26 @@
-MiniDeps.add({ source = 'mistweaverco/kulala.nvim', })
+MiniDeps.add({
+  source = 'mistweaverco/kulala.nvim',
+  depends = { 'nvim-treesitter/nvim-treesitter' },
+})
 
 MiniDeps.now(function()
+  require('kulala').setup({
+    global_keymaps = false,
+    global_keymaps_prefix = '<leader>k',
+    kulala_keymaps_prefix = '',
+  })
+
+  local keymap = vim.keymap
+
+  keymap.set('n', '<leader>ks', function()
+    require('kulala').run()
+  end, { desc = 'Send request' })
+
+  keymap.set('n', '<leader>ka', function()
+    require('kulala').run_all()
+  end, { desc = 'Send all requests' })
+
+  keymap.set('n', '<leader>kb', function()
+    require('kulala').scratchpad()
+  end, { desc = 'Open scratchpad' })
 end)
