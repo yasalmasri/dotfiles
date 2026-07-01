7c84f8fa9bb5ddf2a332fb902bb03e042a693362
Yaser Almasri <yaser@masys.mx>
2026-06-30 21:41:13 -0600

    fix treesitter

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,15 +1,28 @@
 MiniDeps.add({
   source = 'nvim-treesitter/nvim-treesitter',
-  -- Use 'master' while monitoring updates in 'main'
-  checkout = 'master',
-  monitor = 'main',
+  checkout = 'main',
   -- Perform action after every checkout
-  hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
+  hooks = { post_checkout = function()
+    if vim.fn.executable('tree-sitter') == 1 then
+      vim.cmd('TSUpdate')
+    end
+  end },
 })
+
+local languages = { 'lua', 'vimdoc', 'ruby', 'http' }
+
 MiniDeps.now(function()
-  -- Possible to immediately execute code which depends on the added plugin
-  require('nvim-treesitter.configs').setup({
-    ensure_installed = { 'lua', 'vimdoc', 'ruby', 'http' },
-    highlight = { enable = true },
+  require('nvim-treesitter').setup()
+
+  if vim.fn.executable('tree-sitter') == 1 then
+    require('nvim-treesitter').install(languages)
+  end
+
+  vim.api.nvim_create_autocmd('FileType', {
+    group = vim.api.nvim_create_augroup('TreesitterHighlight', { clear = true }),
+    pattern = languages,
+    callback = function()
+      pcall(vim.treesitter.start)
+    end,
   })
 end)
